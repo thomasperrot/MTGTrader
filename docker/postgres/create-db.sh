9a0c7e781d151aac2b0a3939d0b75d5edea39701
Thomas Perrot <thomas.perrot@cybelangel.com>
2017-07-08 15:24:14 +0200

    Added docker features for raspberrypi

--- docker/postgres/create-db.sh
+++ docker/postgres/create-db.sh
@@ -4,12 +4,12 @@
 sleep 10
 
 ## Uses the user postgres, the only one that can currently run psql command
-#su postgres
+su postgres
 ## Creates the database mtg
-#psql --command "CREATE DATABASE mtg;"
+psql --command "CREATE DATABASE mtg;"
 ## Creates user mtg and grant him privileges on mtg database
-#psql --command "CREATE USER mtg WITH PASSWORD 'mtgmtg';"
-#psql --command "ALTER ROLE mtg SET client_encoding TO 'utf8';"
-#psql --command "ALTER ROLE mtg SET default_transaction_isolation TO 'read committed';"
-#psql --command "ALTER ROLE mtg SET timezone TO 'UTC';"
-#psql --command "GRANT ALL PRIVILEGES ON DATABASE mtg TO mtg;"
+psql --command "CREATE USER mtg WITH PASSWORD 'mtgmtg';"
+psql --command "ALTER ROLE mtg SET client_encoding TO 'utf8';"
+psql --command "ALTER ROLE mtg SET default_transaction_isolation TO 'read committed';"
+psql --command "ALTER ROLE mtg SET timezone TO 'UTC';"
+psql --command "GRANT ALL PRIVILEGES ON DATABASE mtg TO mtg;"
